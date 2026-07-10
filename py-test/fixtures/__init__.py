8e3f3781e276db29d9e3748094207ab1d9418d06
HandyWote <huangyinghui01@corp.netease.com>
2026-07-10 17:56:12 +0800

    delete test files

--- py-test/fixtures/__init__.py
+++
@@ -1,8 +0,0 @@
-from .fim_templates_data import FIM_TEMPLATE_CASES, FIM_AUTO_CASES, STOP_WORDS_CASES, REPO_LEVEL_DATA
-from .cache_data import CACHE_TEST_DATA
-from .completion_data import (
-    PREFIX_SUFFIX_CASES,
-    FORMATTER_CASES,
-    MOCK_LLM_CHUNKS,
-    COMPLETION_CHAIN_CASES,
-)
